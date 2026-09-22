2ebaa02fb55617a9f9381544021f3cd1c4f3bdd7
peshkovaea <peshkovaea@mer.ci.nsu.ru>
2026-09-22 11:32:32 +0700

    changes

--- triangle.py
+++ triangle.py
@@ -1,5 +1,25 @@
 import math
+import logging
+import sys
 
+# Шаблон строки лога (аналог template в Serilog)
+# Содержит: время, уровень (до 7 символов для выравнивания), имя логгера и сообщение
+log_format = "%(asctime)s | [%(levelname)-7s] | %(message)s"
+date_format = "%Y-%m-%d %H:%M:%S"
+
+# Базовая настройка корневого логгера
+logging.basicConfig(
+    level=logging.DEBUG, # Минимальный уровень логирования (аналог MinimumLevel.Debug)
+    format=log_format,
+    datefmt=date_format,
+    handlers=[
+        logging.StreamHandler(sys.stdout),          # Настройка логирования в консоль
+        logging.FileHandler("logs/file_txt.log", encoding="utf-8") # Настройка логирования в файл
+    ]
+)
+
+logging.info("Логгер успешно сконфигурирован")
+logging.info("Приложение запущено")
 
 def parse_side(s):
     #преобразуем строку в положительное float, если нет то None
